ecc39deeaf29760db7030d4ab62badbb99ad2be1
vdkptt <Zigaman13@gmail.com>
2022-02-11 19:02:03 +0800

    init

---
+++ src/post/entities/post.entity.ts
@@ -0,0 +1,42 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+} from 'typeorm';
+import { OutputBlockData } from '../dto/create-post.dto';
+import { UserEntity } from '../../user/entities/user.entity';
+
+@Entity('posts')
+export class PostEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column({ type: 'jsonb' })
+  body: OutputBlockData[];
+
+  @Column()
+  description: string;
+
+  @ManyToOne(() => UserEntity, { eager: true })
+  user: UserEntity;
+
+  @Column({
+    default: 0,
+  })
+  views: number;
+
+  @Column({ nullable: true })
+  tags?: string;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamp' })
+  updatedAt: Date;
+}
